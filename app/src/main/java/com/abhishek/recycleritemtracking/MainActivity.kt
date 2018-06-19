6304bc463ec172610eb362eb2eb3f64b48bb6170
Abhishek Bharadwaj <abhishek.bharadwaj@healthifyme.com>
2018-06-19 23:54:39 +0530

    first commit

---
+++ app/src/main/java/com/abhishek/recycleritemtracking/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.abhishek.recycleritemtracking
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.layout_item.view.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val words = resources.getStringArray(R.array.words)
+        rv.layoutManager = LinearLayoutManager(this)
+        rv.adapter = Adapter(this, words)
+
+    }
+
+    class Adapter(private val context: Context, private val words: Array<String>) :
+        RecyclerView.Adapter<VH>() {
+
+        private val inflater = LayoutInflater.from(context)
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+            return VH(inflater.inflate(R.layout.layout_item, parent, false))
+        }
+
+        override fun getItemCount() = words.size
+
+        override fun onBindViewHolder(holder: VH, position: Int) {
+            holder.itemView.tv.text = words[position]
+        }
+    }
+
+    class VH(rootView: View) : RecyclerView.ViewHolder(rootView)
+}
