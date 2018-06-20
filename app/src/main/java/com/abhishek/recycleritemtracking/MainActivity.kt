5f123b4789be424eb508c24b4ee6ceff66d701fe
Abhishek Bharadwaj <abhishek.bharadwaj@healthifyme.com>
2018-06-20 22:23:27 +0530

    Done

--- app/src/main/java/com/abhishek/recycleritemtracking/MainActivity.kt
+++ app/src/main/java/com/abhishek/recycleritemtracking/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,16 +13,16 @@ import android.widget.Toast
 import io.reactivex.Completable
 import io.reactivex.CompletableObserver
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.layout_item.view.*
 import java.util.*
 import java.util.concurrent.TimeUnit
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
-    private val compositeDisposable = CompositeDisposable()
+    private lateinit var listDisposable: MutableList<Disposable?>
     private lateinit var layoutManager: LinearLayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,17 +30,48 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val words = resources.getStringArray(R.array.words)
+        listDisposable = ArrayList(words.size)
+        for (i in 0..words.size) listDisposable.add(null)
         layoutManager = LinearLayoutManager(this)
         rv.layoutManager = layoutManager
         rv.adapter = Adapter(this, words)
+
+        rv.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView?, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                val firstVisible = layoutManager.findFirstVisibleItemPosition()
+                val lastVisible = layoutManager.findLastVisibleItemPosition()
+                for (i in 0 until listDisposable.size) {
+                    if (i in firstVisible..lastVisible) {
+                        if (listDisposable[i] == null) createEventCompletable(i)
+                    } else {
+                        listDisposable[i]?.dispose()
+                        listDisposable[i] = null
+                    }
+                }
+            }
+        })
     }
 
-    override fun onDestroy() {
-        compositeDisposable.clear()
-        super.onDestroy()
+    private fun createEventCompletable(position: Int) {
+        Completable.timer(300, TimeUnit.MILLISECONDS, AndroidSchedulers.mainThread())
+            .subscribe(object : CompletableObserver {
+                override fun onComplete() {
+                    Toast.makeText(this@MainActivity,
+                        "Event send for $position", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onSubscribe(d: Disposable) {
+                    listDisposable.set(position, d)
+                }
+
+                override fun onError(e: Throwable) {
+
+                }
+            })
     }
 
-    inner class Adapter(private val context: Context, private val words: Array<String>) :
+    inner class Adapter(context: Context, private val words: Array<String>) :
         RecyclerView.Adapter<VH>() {
 
         private val rnd = Random()
@@ -54,32 +84,10 @@ class MainActivity : AppCompatActivity() {
         override fun getItemCount() = words.size
 
         override fun onBindViewHolder(holder: VH, position: Int) {
-            holder.itemView.tv.text = words[position]
+            holder.itemView.tv.text = "$position : ${words[position]}"
             val color = Color.argb(255,
                 rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256))
             holder.itemView.setBackgroundColor(color)
-
-            Log.e("OOOOOOO", "visible item $position")
-            Completable.timer(300, TimeUnit.MILLISECONDS, AndroidSchedulers.mainThread())
-                .subscribe(object : CompletableObserver {
-                    override fun onComplete() {
-                        val firstVisible = layoutManager.findFirstVisibleItemPosition()
-                        val lastVisible = layoutManager.findLastVisibleItemPosition()
-                        val currentPosition = holder.adapterPosition
-                        if (currentPosition in firstVisible..lastVisible) {
-                            Toast.makeText(this@MainActivity,
-                                "Sending event for $currentPosition", Toast.LENGTH_SHORT).show()
-                        }
-                    }
-
-                    override fun onSubscribe(d: Disposable) {
-                        compositeDisposable.add(d)
-                    }
-
-                    override fun onError(e: Throwable) {
-
-                    }
-                })
         }
     }
 
